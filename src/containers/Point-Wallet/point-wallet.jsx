15e151acf5a0188f52e30af173c980de0f98f592
Will Stiles <willstiles@Wills-MacBook-Pro.local>
2024-03-29 04:38:59 -0400

    added rolling animation to point counter

--- src/containers/Point-Wallet/point-wallet.jsx
+++ src/containers/Point-Wallet/point-wallet.jsx
@@ -1,11 +1,27 @@
-import './point-wallet.scss';
+import "./point-wallet.scss";
 
-
-function PointWallet({wallet}) {
+function PointWallet({ wallet }) {
+  let numArray = [0, 1, 2, 3, 4, 5, 6];
   return (
     <div className="point-wallet-wrapper">
-        <div className='ratio'>{`${wallet.spent} / ${wallet.total}`}</div>
-        <h3>Points Spent</h3>
+      <div className="ratio">
+        <div className="spent-wrapper">
+          {numArray.map((num) => {
+            return (
+              <p
+                style={{ // This function decides what number to display, allows for counter animation 
+                  transform: `translateY(-${
+                    wallet.spent * 77 - (num > 4 ? 2 : 0)
+                  }px)`,
+                }}
+                className={`spent ${num === wallet.spent && "spent-active"}`}
+              >{`${num}`}</p>
+            );
+          })}
+        </div>
+        <p className="total">{`/ ${wallet.total}`}</p>
+      </div>
+      <h3>Points Spent</h3>
     </div>
   );
 }
